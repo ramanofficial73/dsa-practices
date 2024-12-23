cdbcd9e0904ac909b7e305b1bd9bab7b6a9904f4
ramanofficial73 <rksraman73@gmail.com>
2024-12-23 21:34:00 +0530

    Adding more qustion for recurion

--- src/main/java/com/dsa/practices/dec2024/dec23/DSA_Recursion_Pratices.java
+++ src/main/java/com/dsa/practices/dec2024/dec23/DSA_Recursion_Pratices.java
@@ -4,8 +4,46 @@ public class DSA_Recursion_Pratices {
     public static void main(String[] args) {
 //        System.out.println(getFactorial(5));
 
-        System.out.println("Natural Number sum : " + getNaturalNumberSum(10));
+//        System.out.println("Natural Number sum : " + getNaturalNumberSum(10));
 
+//        int n = 10;
+//        for (int i = 0; i <= n; i++) {
+//            System.out.print(" " + fibonacci(i));
+//        }
+
+//        String name = "raman";
+//        System.out.println("Reverse String : " + reverseString(name, name.length() - 1));
+
+        String name1 = "ma2dam";
+        System.out.println(checkPolindrom(name1, 0, name1.length() - 1));
+
+    }
+
+    private static boolean checkPolindrom(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return checkPolindrom(str, start + 1, end - 1);
+    }
+
+    private static String reverseString(String name, int index) {
+        if (index < 0) {
+            return "";
+        }
+        return name.charAt(index) + reverseString(name, index - 1);
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
 
