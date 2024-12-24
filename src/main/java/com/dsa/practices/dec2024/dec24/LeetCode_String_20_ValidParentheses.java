e4404eafe950f5aff70effc1ae86f1ca2f28c188
ramanofficial73 <rksraman73@gmail.com>
2024-12-24 23:39:56 +0530

    Sloved leet code 20

--- src/main/java/com/dsa/practices/dec2024/dec24/LeetCode_String_20_ValidParentheses.java
+++ src/main/java/com/dsa/practices/dec2024/dec24/LeetCode_String_20_ValidParentheses.java
@@ -4,11 +4,31 @@ import java.util.Stack;
 
 public class LeetCode_String_20_ValidParentheses {
     public static void main(String[] args) {
-
+        System.out.println(isValid("(]"));
     }
 
     public static boolean isValid(String s) {
         String[] strings = s.split("");
-        Stack<>
+        Stack<String> stack = new Stack<>();
+
+        for (String data : strings) {
+            if (data.equals("{") || data.equals("[") || data.equals("(")) {
+                stack.push(data);
+            } else {
+                if (data.equals("]") || data.equals("}") || data.equals(")")) {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+
+                    String lastElement = stack.pop();
+                    if (data.equals("}") && !lastElement.equals("{") ||
+                            data.equals("]") && !lastElement.equals("[") ||
+                            data.equals(")") && !lastElement.equals("(")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
