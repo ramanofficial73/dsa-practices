bd445bae459abf53631277cc251a477ac46d60a9
ramanofficial73 <rksraman73@gmail.com>
2024-12-24 23:33:38 +0530

    Adding 20

---
+++ src/main/java/com/dsa/practices/dec2024/dec24/LeetCode_String_20_ValidParentheses.java
@@ -0,0 +1,14 @@
+package com.dsa.practices.dec2024.dec24;
+
+import java.util.Stack;
+
+public class LeetCode_String_20_ValidParentheses {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValid(String s) {
+        String[] strings = s.split("");
+        Stack<>
+    }
+}
--- src/main/java/com/dsa/practices/dec2024/dec24/LeetCode_String_28_FindFirstIndexOfString.java
+++ src/main/java/com/dsa/practices/dec2024/dec24/LeetCode_String_28_FindFirstIndexOfString.java
@@ -1,5 +1,12 @@
 package com.dsa.practices.dec2024.dec24;
 
 public class LeetCode_String_28_FindFirstIndexOfString {
+    public static void main(String[] args) {
+        String haystack = "sadbutsad", needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
 
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
 }
