93d88fd35f9bab287243b68513a52fc942c3871d
ramanofficial73 <rksraman73@gmail.com>
2024-12-28 21:35:03 +0530

    Dolid principle

---
+++ src/main/java/com/dsa/practices/dec2024/dec27/SolidPrinciple.java
@@ -0,0 +1,52 @@
+package com.dsa.practices.dec2024.dec27;
+
+public class SolidPrinciple {
+
+    /*
+        SOLID is a set of five principles in object-oriented programming designed to make code
+        more maintainable, scalable, and robust.
+
+        1. S - Single Responsibility Principle (SRP)
+        Def: A class should have only one reason to change, meaning it should only have one responsibility.
+        Key Idea: Each class should focus on a single task or functionality to ensure modularity
+                  and reduce the risk of unexpected side effects.
+
+        2. O - Open/Closed Principle (OCP)
+        Def: Software entities (classes, modules, functions) should be open for extension
+             but closed for modification.
+        Key Idea: You should be able to add new functionality without altering existing
+                  code to minimize risk.
+
+        3. L - Liskov Substitution Principle (LSP)
+        Def: Objects of a superclass should be replaceable with objects of a subclass without
+             affecting the correctness of the program.
+        Key Idea: A subclass should not violate the behavior or expectations set by its parent class.
+
+        4. I - Interface Segregation Principle (ISP)
+        Def: A class should not be forced to implement interfaces it does not use.
+        Key Idea: Split large interfaces into smaller, more specific ones so that implementing
+                classes only need to know about methods relevant to them.
+
+         5. D - Dependency Inversion Principle (DIP)
+         Def: High-level modules should not depend on low-level modules. Both should depend on abstractions.
+              Abstractions should not depend on details; details should depend on abstractions.
+        Key Idea: Depend on interfaces or abstractions rather than concrete implementations.
+
+        Benefits of Following SOLID Principles:
+        1. Improved Code Maintainability: Easier to modify and extend the code.
+        2. Scalability: Better adaptability to new requirements.
+        3. Reusability: Classes and modules can be reused in different contexts.
+        4. Testability: Promotes writing testable code by reducing coupling.
+        5. Readability: Clear and well-organized code.
+
+        Quick Summary:
+        SRP: One class, one responsibility.
+        OCP: Extend, don’t modify.
+        LSP: Subclasses replace base classes.
+        ISP: Split interfaces; no unused methods.
+        DIP: Depend on abstractions, not implementations.
+
+
+     */
+
+}
