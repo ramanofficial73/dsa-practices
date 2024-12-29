440835bbfa29936b68512e3b742a1406bf96033b
ramanofficial73 <rksraman73@gmail.com>
2024-12-29 22:53:33 +0530

    Power of four - leetcode

---
+++ src/main/java/com/dsa/practices/dec2024/dec29/LeetCode_ArrayList_326_PowerOfThree.java
@@ -0,0 +1,24 @@
+package com.dsa.practices.dec2024.dec29;
+
+public class LeetCode_ArrayList_326_PowerOfThree {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(20));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        } else if (n == 1) {
+            return true;
+        }
+
+        while (n % 4 == 0) {
+            n = n / 4;
+        }
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
