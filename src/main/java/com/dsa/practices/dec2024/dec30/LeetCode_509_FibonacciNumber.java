e0bf17e64d31f2c303b8928925003fdab87137ae
ramanofficial73 <rksraman73@gmail.com>
2024-12-30 22:41:03 +0530

    Adding Fibonacci Number

--- src/main/java/com/dsa/practices/dec2024/dec30/LeetCode_509_FibonacciNumber.java
+++ src/main/java/com/dsa/practices/dec2024/dec30/LeetCode_509_FibonacciNumber.java
@@ -1,4 +1,25 @@
 package com.dsa.practices.dec2024.dec30;
 
 public class LeetCode_509_FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fib(2));
+    }
+
+    public static int fib(int n) {
+        if (n== 0) {
+            return 0;
+        } else if (n ==1) {
+            return 1;
+        }
+
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        for (int i=1; i<=n; i++) {
+            int thirdTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+        }
+        return firstTerm;
+    }
 }
