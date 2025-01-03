1ec5b3080c7e5fd56a2983d8a66737cdfabc4bb2
ramanofficial73 <rksraman73@gmail.com>
2025-01-03 22:17:29 +0530

    Leecode 268

---
+++ src/main/java/com/dsa/practices/jan2024/jan01/LeetCode_ArrayList_268_MissingNumber.java
@@ -0,0 +1,36 @@
+package com.dsa.practices.jan2024.jan01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LeetCode_ArrayList_268_MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSum = (range * (range + 1))/2;
+        int currentSum = 0;
+        for (int num: nums) {
+            currentSum += num;
+        }
+        int ans = actualSum - currentSum;
+        return ans;
+        /*
+        int length = nums.length;
+        int i = 0;
+        List<Integer> data = IntStream.of(nums).boxed()
+                .collect(Collectors.toList());
+        for (i = 0; i <= length; i++) {
+            if (!data.contains(i)) {
+                return i;
+            }
+        }
+        return i;
+        */
+    }
+}
