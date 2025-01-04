d98bfbd929fde445c01ad938e94e190b5d4e0b49
ramanofficial73 <rksraman73@gmail.com>
2025-01-04 19:16:38 +0530

    334. Increasing Triplet Subsequence

--- src/main/java/com/dsa/practices/jan2024/jan04/LeetCode_ArrayList_747_LargestNumberAtLeastTwiceOthers.java
+++ src/main/java/com/dsa/practices/jan2024/jan04/LeetCode_ArrayList_747_LargestNumberAtLeastTwiceOthers.java
@@ -2,14 +2,14 @@ package com.dsa.practices.jan2024.jan04;
 
 public class LeetCode_ArrayList_747_LargestNumberAtLeastTwiceOthers {
     public static void main(String[] args) {
-        int[] data = {3,6,1,0};
+        int[] data = {3, 6, 1, 0};
         System.out.println(dominantIndex(data));
     }
 
     public static int dominantIndex(int[] nums) {
         int maxValue = 0;
         int index = -1;
-        for (int i=0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] > maxValue) {
                 maxValue = nums[i];
                 index = i;
