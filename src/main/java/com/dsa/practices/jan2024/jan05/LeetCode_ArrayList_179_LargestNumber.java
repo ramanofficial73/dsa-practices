1180f0ab9e036331a48316d3fd035d163769070b
ramanofficial73 <rksraman73@gmail.com>
2025-01-05 13:38:55 +0530

    179. Largest Number

--- src/main/java/com/dsa/practices/jan2024/jan05/LeetCode_ArrayList_179_LargestNumber.java
+++ src/main/java/com/dsa/practices/jan2024/jan05/LeetCode_ArrayList_179_LargestNumber.java
@@ -10,10 +10,16 @@ public class LeetCode_ArrayList_179_LargestNumber {
 
     public static String largestNumber(int[] nums) {
         String[] data = new String[nums.length];
+
+        /* Converting into string */
         for (int i = 0; i < nums.length; i++) {
             data[i] = nums[i] + "";
         }
+
+        /* Sorting as string*/
         Arrays.sort(data, (s1, s2) -> (s1 + s2).compareTo(s2 + s1));
+
+        /* adding in reverse order */
         StringBuilder builder = new StringBuilder("");
         for (int i = data.length - 1; i >= 0; i--) {
             builder.append(data[i]);
