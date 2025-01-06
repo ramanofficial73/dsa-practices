087c1073c077db3c0056cfb1d393b9e94f2a2553
ramanofficial73 <rksraman73@gmail.com>
2025-01-06 09:18:31 +0530

    Practices_ReverseStringUsingStack

---
+++ src/main/java/com/dsa/practices/jan2024/jan06/Practices_ReverseStringUsingStack.java
@@ -0,0 +1,22 @@
+package com.dsa.practices.jan2024.jan06;
+
+import java.util.Stack;
+
+public class Practices_ReverseStringUsingStack {
+    public static void main(String[] args) {
+        System.out.println(reverseStr("raman"));
+    }
+
+    public static String reverseStr(String str) {
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            stack.push(c);
+        }
+        StringBuilder builder = new StringBuilder();
+        while (stack.size() > 0) {
+            builder.append(stack.pop());
+        }
+        return builder.toString();
+    }
+}
