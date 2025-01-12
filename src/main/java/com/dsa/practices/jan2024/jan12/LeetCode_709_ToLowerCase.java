a869a7b4370bc01bc2c6c8fb234a8825116d396f
ramanofficial73 <rksraman73@gmail.com>
2025-01-12 14:01:16 +0530

    709. To Lower Case

---
+++ src/main/java/com/dsa/practices/jan2024/jan12/LeetCode_709_ToLowerCase.java
@@ -0,0 +1,20 @@
+package com.dsa.practices.jan2024.jan12;
+
+public class LeetCode_709_ToLowerCase {
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("raMAN"));
+    }
+
+    public static String toLowerCase(String s) {
+        char[] c = s.toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+            /* Checking with asci Values for Capital letter 65 to 90*/
+            if (c[i] >= 65 && c[i] <= 90) {
+                c[i] = (char) (c[i] + 32);   //For adding asci value to lower case
+            }
+        }
+        return String.valueOf(c);
+    }
+}
