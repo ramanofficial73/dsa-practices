c105227c5bfb80d9ecc9810627622a388fbf3948
ramanofficial73 <rksraman73@gmail.com>
2025-01-13 21:20:31 +0530

    162. Find Peak Element

---
+++ src/main/java/com/dsa/practices/jan2024/jan13/LeetCode_162_FindPeakElement.java
@@ -0,0 +1,27 @@
+package com.dsa.practices.jan2024.jan13;
+
+public class LeetCode_162_FindPeakElement {
+    public static void main(String[] args) {
+        int[] data = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(data));
+        
+    }
+
+    public static int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+        for (int i=1; i< nums.length-1; i++) {
+            if (nums[i-1] < nums[i] && nums[i] > nums[i+1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
