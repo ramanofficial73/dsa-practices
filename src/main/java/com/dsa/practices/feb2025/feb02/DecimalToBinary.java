62b263ca4728ef818758e60f85dce1b5da43d1dd
ramanofficial73 <rksraman73@gmail.com>
2025-02-02 22:36:53 +0530

    BinaryToDecimal

---
+++ src/main/java/com/dsa/practices/feb2025/feb02/DecimalToBinary.java
@@ -0,0 +1,14 @@
+package com.dsa.practices.feb2025.feb02;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int data = 5;
+        StringBuilder sb = new StringBuilder();
+        while (data > 0) {
+            int rem = data % 2;   // Get remainder (0 or 1)
+            sb.append(rem);       // Append to binary string
+            data = data / 2;      // Divide by 2
+        }
+        System.out.println(sb);
+    }
+}
