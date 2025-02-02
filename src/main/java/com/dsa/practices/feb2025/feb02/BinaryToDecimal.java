c1c685b41fe0260b773819fd339a0fde74bf80c7
ramanofficial73 <rksraman73@gmail.com>
2025-02-02 22:37:40 +0530

    BinaryToDecimal

--- src/main/java/com/dsa/practices/feb2025/feb02/BinaryToDecimal.java
+++ src/main/java/com/dsa/practices/feb2025/feb02/BinaryToDecimal.java
@@ -2,13 +2,13 @@ package com.dsa.practices.feb2025.feb02;
 
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        int data = 5;
-        StringBuilder sb = new StringBuilder();
-        while (data > 0) {
-            int rem = data % 2;   // Get remainder (0 or 1)
-            sb.append(rem);       // Append to binary string
-            data = data / 2;      // Divide by 2
+        String binary = "101";
+        int decimal = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            char ch = binary.charAt(i); // Get character ('0' or '1')
+            int num = ch - '0';
+            decimal = decimal * 2 + num;
         }
-        System.out.println(sb);
+        System.out.println(decimal);
     }
 }
