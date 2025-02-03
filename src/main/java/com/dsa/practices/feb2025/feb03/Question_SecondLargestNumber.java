7f841cdb10532c895eba3405fab410909f61eb69
ramanofficial73 <rksraman73@gmail.com>
2025-02-03 22:07:23 +0530

    Question_SecondLargestNumber

---
+++ src/main/java/com/dsa/practices/feb2025/feb03/Question_SecondLargestNumber.java
@@ -0,0 +1,40 @@
+package com.dsa.practices.feb2025.feb03;
+
+public class Question_SecondLargestNumber {
+    public static void main(String[] args) {
+        int[] data = {1, 2, 4, 5, 6, 5};
+//        int[] data = {1, 2, 2, 2, 2, 2};
+        System.out.println(getSecondLarges(data));
+    }
+
+    /* Solution first */
+    /*
+    public static int getSecondLarges(int[] nums) {
+        Arrays.sort(nums);
+        int largest = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (largest != nums[i]) {
+                return nums[i];
+            }
+        }
+        return largest;
+    }
+    */
+
+    public static int getSecondLarges(int[] nums) {
+        int largest = nums[0];
+        for (int i = 1; i <= nums.length - 1; i++) {
+            if (largest < nums[i]) {
+                largest = nums[i];
+            }
+        }
+
+        int secLargest = -1;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (secLargest < nums[i] && nums[i] != largest) {
+                secLargest = nums[i];
+            }
+        }
+        return secLargest;
+    }
+}
