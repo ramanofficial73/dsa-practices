23b88a8c0a585fde870d515e2f8af184dc16f296
ramanofficial73 <rksraman73@gmail.com>
2025-02-06 21:44:34 +0530

    Question_MoveElementInAnotherStackInSameOrder

---
+++ src/main/java/com/dsa/practices/feb2025/stack/Question_MoveElementInAnotherStackInSameOrder.java
@@ -0,0 +1,24 @@
+package com.dsa.practices.feb2025.stack;
+
+import java.util.Stack;
+
+public class Question_MoveElementInAnotherStackInSameOrder {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("Stack Element : " + stack);
+
+        Stack<Integer> temp = new Stack<>();
+        while (!stack.isEmpty()) {
+            temp.push(stack.pop());
+        }
+
+        Stack<Integer> copyStack = new Stack<>();
+        while (!temp.isEmpty()) {
+            copyStack.push(temp.pop());
+        }
+        System.out.println("After Moved Stack Element : " + copyStack);
+    }
+}
---
+++ src/main/java/com/dsa/practices/feb2025/stack/StackExample.java
@@ -0,0 +1,17 @@
+package com.dsa.practices.feb2025.stack;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+    }
+}
