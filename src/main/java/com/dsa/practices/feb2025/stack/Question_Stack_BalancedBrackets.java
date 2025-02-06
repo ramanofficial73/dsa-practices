8d0add16067d9eb81dc2ebf572ab3a7085c85adc
ramanofficial73 <rksraman73@gmail.com>
2025-02-06 22:37:28 +0530

    Question_Stack_BalancedBrackets

---
+++ src/main/java/com/dsa/practices/feb2025/stack/Question_Stack_BalancedBrackets.java
@@ -0,0 +1,36 @@
+package com.dsa.practices.feb2025.stack;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Question_Stack_BalancedBrackets {
+    public static void main(String[] args) {
+        String data = "}{{[]}}";
+        System.out.println(isValid(data));
+    }
+
+    private static boolean isValid(String data) {
+        List<String> listsOfEle = Arrays.asList(data.split(""));
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i <= listsOfEle.size() - 1; i++) {
+            if (listsOfEle.get(i).equals("{")
+                    || listsOfEle.get(i).equals("(")
+                    || listsOfEle.get(i).equals("[")) {
+                stack.push(listsOfEle.get(i));
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    String lastElement = stack.pop();
+                    if (data.equals("}") && !lastElement.equals("{") ||
+                            data.equals("]") && !lastElement.equals("[") ||
+                            data.equals(")") && !lastElement.equals("(")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
