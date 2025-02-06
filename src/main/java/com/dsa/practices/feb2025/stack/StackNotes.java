5cd1794feb76e9f285a86be8aab801aac5d673c4
ramanofficial73 <rksraman73@gmail.com>
2025-02-06 21:30:56 +0530

    StackNotes

---
+++ src/main/java/com/dsa/practices/feb2025/stack/StackNotes.java
@@ -0,0 +1,34 @@
+package com.dsa.practices.feb2025.stack;
+
+public class StackNotes {
+
+    /*
+    A stack is a linear data structure that follows the Last In, First Out (LIFO) principle.
+    This means the last element added to the stack is the first one to be removed.
+
+    Basic Operations
+    #1. Push → Adds an element to the top of the stack.
+    #2. Pop → Removes and returns the top element.
+    #3. Peek (or Top) → Returns the top element without removing it.
+    #4. isEmpty → Checks if the stack is empty.
+
+    Use Cases of Stack
+    #1. Function Call Stack → Used in recursion and method execution.
+    #2. Undo/Redo Operations → Used in text editors and applications.
+    #3. Expression Evaluation → Used in balancing parentheses, postfix evaluation, etc.
+    #4. Depth-First Search (DFS) → Used in graph traversal.
+
+    Stack Implementation Types
+    1. Using Arrays → Fixed size, efficient access.
+    2. Using Linked List → Dynamic size, extra memory for pointers.
+
+    Key Characteristics
+    #1. LIFO Principle → The last element inserted is the first one to be removed.
+    #2. Dynamic Size → Can be implemented using arrays or linked lists.
+    #3. Efficient Operations → Push and pop operations have O(1) time complexity.
+    #4. Limited Access → Only the top element can be accessed directly.
+    #5. Memory Utilization → Linked list implementation takes extra memory for pointers.
+
+
+    */
+}
