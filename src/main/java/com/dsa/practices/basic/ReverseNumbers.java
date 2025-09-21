2adf48eff220ab7eb15e0a370fdec76c4d5f41ab
ramanofficial73 <rksraman73@gmail.com>
2025-09-21 14:13:03 +0530

    ReverseNumbers

---
+++ src/main/java/com/dsa/practices/basic/ReverseNumbers.java
@@ -0,0 +1,14 @@
+package com.dsa.practices.basic;
+
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        int n = 124424999;
+        int reverseNumber = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            n = n / 10;
+            reverseNumber = (reverseNumber * 10) + lastDigit;
+        }
+        System.out.println("Reverse numbers : " + reverseNumber);
+    }
+}
