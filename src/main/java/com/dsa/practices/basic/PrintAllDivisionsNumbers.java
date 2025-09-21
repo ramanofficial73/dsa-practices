883b336f756b25b9398166126b7bf660eacafa64
ramanofficial73 <rksraman73@gmail.com>
2025-09-21 18:51:52 +0530

    PrintAllDivisionsNumbers

--- src/main/java/com/dsa/practices/basic/PrintAllDivisionsNumbers.java
+++ src/main/java/com/dsa/practices/basic/PrintAllDivisionsNumbers.java
@@ -3,7 +3,8 @@ package com.dsa.practices.basic;
 public class PrintAllDivisionsNumbers {
     public static void main(String[] args) {
         int n = 500;
-        getDivisionNumbers(n);
+//        getDivisionNumbers(n);
+        getDivisionNumbersWithOSqrtN(n);
     }
 
     /* Time Complexity - O(n) */
@@ -11,6 +12,18 @@ public class PrintAllDivisionsNumbers {
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 System.out.println(i);
+            }
+        }
+    }
+
+    /* Time Complexity - (O(√n) */
+    private static void getDivisionNumbersWithOSqrtN(int num) {
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+                if (i != num / i) {
+                    System.out.print(num / i + " ");
+                }
             }
         }
     }
